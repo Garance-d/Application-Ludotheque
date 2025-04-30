a74f9796c31107bdeb8da40ac3a20547eb06964f
Garance <davidgarance0@gmail.com>
2025-04-30 12:39:55 +0200

    Initialisation du projet

---
+++ src/main/java/fr/eni/applicationludotheque/ApplicationLudothequeApplication.java
@@ -0,0 +1,13 @@
+package fr.eni.applicationludotheque;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationLudothequeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApplicationLudothequeApplication.class, args);
+    }
+
+}
